2ffa62b804e2632d5a4266395365dab928dffd51
Hanna Kruppe <hanna.kruppe@gmail.com>
2024-09-25 22:46:54 +0200

    add next_u64 and benchmarks for it
    Again motivated by comparison to Go and better understanding how the
    cost is split between filling the buffer and consuming it.

--- benches/stupid.rs
+++ benches/stupid.rs
@@ -11,11 +11,20 @@ fn main() {
 const SEED: &[u8; 32] = b"thisisjustabenchthisisjustabench";
 
 #[inline(never)]
-fn bench_next(bencher: Bencher, backend: Backend) {
+fn bench_next_u32(bencher: Bencher, backend: Backend) {
     let backend = black_box(backend);
     let mut rng = ChaCha8::with_backend(Seed::from(SEED), backend);
     bencher.bench_local(|| {
         black_box(rng.next_u32());
+    });
+}
+
+#[inline(never)]
+fn bench_next_u64(bencher: Bencher, backend: Backend) {
+    let backend = black_box(backend);
+    let mut rng = ChaCha8::with_backend(Seed::from(SEED), backend);
+    bencher.bench_local(|| {
+        black_box(rng.next_u64());
     });
 }
 
@@ -37,25 +46,49 @@ fn bench_bulk(bencher: Bencher, backend: Backend) {
 }
 
 #[divan::bench]
-fn next_scalar(bencher: Bencher) {
-    bench_next(bencher, Backend::scalar());
+fn next_u32_scalar(bencher: Bencher) {
+    bench_next_u32(bencher, Backend::scalar());
 }
 
 #[divan::bench]
-fn next_simd128(bencher: Bencher) {
-    bench_next(bencher, Backend::simd128());
-}
-
-#[divan::bench]
-#[cfg(target_arch = "x86_64")]
-fn next_avx2(bencher: Bencher) {
-    bench_next(bencher, Backend::avx2().expect("avx2 is required for this"));
+fn next_u32_simd128(bencher: Bencher) {
+    bench_next_u32(bencher, Backend::simd128());
 }
 
 #[divan::bench]
 #[cfg(target_arch = "x86_64")]
-fn next_nop_backend(bencher: Bencher) {
-    bench_next(
+fn next_u32_avx2(bencher: Bencher) {
+    bench_next_u32(bencher, Backend::avx2().expect("avx2 is required for this"));
+}
+
+#[divan::bench]
+#[cfg(target_arch = "x86_64")]
+fn next_u32_nop(bencher: Bencher) {
+    bench_next_u32(
+        bencher,
+        Backend::totally_wrong_stub_for_testing_that_breaks_everything_if_you_actually_use_it(),
+    );
+}
+#[divan::bench]
+fn next_u64_scalar(bencher: Bencher) {
+    bench_next_u64(bencher, Backend::scalar());
+}
+
+#[divan::bench]
+fn next_u64_simd128(bencher: Bencher) {
+    bench_next_u64(bencher, Backend::simd128());
+}
+
+#[divan::bench]
+#[cfg(target_arch = "x86_64")]
+fn next_u64_avx2(bencher: Bencher) {
+    bench_next_u64(bencher, Backend::avx2().expect("avx2 is required for this"));
+}
+
+#[divan::bench]
+#[cfg(target_arch = "x86_64")]
+fn next_u64_nop(bencher: Bencher) {
+    bench_next_u64(
         bencher,
         Backend::totally_wrong_stub_for_testing_that_breaks_everything_if_you_actually_use_it(),
     );
