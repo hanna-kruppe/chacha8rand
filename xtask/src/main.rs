bc1346ff1399752772b1c36bb1ab61a28a2234f8
Hanna Kruppe <hanna.kruppe@gmail.com>
2024-09-27 20:16:52 +0200

    xtask for testing with cross

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use xflags::xflags;
+use xshell::{cmd, Shell};
+
+xflags! {
+    cmd xtask {
+        cmd crosstest {}
+    }
+}
+
+fn main() -> xshell::Result<()> {
+    match Xtask::from_env_or_exit().subcommand {
+        XtaskCmd::Crosstest(Crosstest {}) => crosstest(),
+    }
+}
+
+fn crosstest() -> xshell::Result<()> {
+    let sh = Shell::new()?;
+    let targets = ["x86_64-unknown-linux-gnu", "aarch64-unknown-linux-gnu"];
+    for target in targets {
+        // Testing the x86_64 target on an x86_64 host means rustflags from $CARGO_HOME/.config.toml
+        // are picked up within the container. This is a problem if those contain
+        // -Clink-arg=-fuse-lld, which doesn't work inside the container. Setting the RUSTFLAGS
+        // variable overrides the flags from the config files.
+        cmd!(sh, "cross test --target {target} --all-targets")
+            .env("RUSTFLAGS", "")
+            .run()?;
+    }
+    Ok(())
 }
